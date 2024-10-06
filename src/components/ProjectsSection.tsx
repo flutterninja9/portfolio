50f3ec22305f51920e3de3bea3ae75fc8327150b
Anirudh <anirudh.singh@thoughtworks.com>
2024-10-06 14:42:09 +0530

    Refactor ProjectsSection component layout and styling

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -13,36 +13,35 @@ export default async function ProjectsSection() {
       <h2 className="text-2xl font-bold mb-4">My Projects</h2>
       <div className="space-y-8">
         {projects.map((project) => (
-          <Card key={project.id} className="overflow-hidden p-2 h-[400px]">
-            <div className="md:flex h-full">
-              <div className="md:w-1/3 relative h-full">
-                {/* Aspect Ratio Wrapper */}
-                <div className="relative w-full h-full aspect-[3/2]"> {/* Adjust aspect ratio as needed */}
-                  <Image
-                    src={project.screenShotUrl}
-                    alt={`${project.name} screenshot`}
-                    layout="fill"
-                    objectFit="contain"
-                    className="rounded-md"
-                  />
-                </div>
+          <Card key={project.id} className="overflow-hidden">
+            <div className="flex flex-col lg:flex-row md:flex-row">
+              <div className="my-4 lg:my-2 w-full lg:w-1/3 relative h-[280px] lg:h-[280px]">
+                <Image
+                  src={project.screenShotUrl}
+                  alt={`${project.name} screenshot`}
+                  layout="fill"
+                  objectFit="contain"
+                  className="rounded-t-lg lg:rounded-l-lg lg:rounded-t-none"
+                />
               </div>
-              <div className="md:w-2/3 p-6 flex flex-col justify-start">
-                <CardHeader className="p-0 mb-4">
-                  <CardTitle className="text-2xl">{project.name}</CardTitle>
-                  <CardDescription>{project.description}</CardDescription>
-                </CardHeader>
-                <CardContent className="p-0">
-                  <p className="text-muted-foreground mb-0">
-                    {project.longDescription}
-                  </p>
-                  <div className="flex flex-wrap gap-2 mb-4">
-                    {project.tags.map((tag, index) => (
-                      <Badge key={index} variant="secondary">{tag}</Badge>
-                    ))}
-                  </div>
-                </CardContent>
-                <CardFooter className="p-0 pt-4 flex justify-start space-x-4">
+              <div className="w-full lg:w-2/3 p-6 flex flex-col justify-between">
+                <div>
+                  <CardHeader className="p-0 mb-4">
+                    <CardTitle className="text-2xl">{project.name}</CardTitle>
+                    <CardDescription>{project.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent className="p-0">
+                    <p className="text-muted-foreground mb-4">
+                      {project.longDescription}
+                    </p>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                      {project.tags.map((tag, index) => (
+                        <Badge key={index} variant="secondary">{tag}</Badge>
+                      ))}
+                    </div>
+                  </CardContent>
+                </div>
+                <CardFooter className="p-0 pt-4 flex flex-wrap gap-4">
                   <Button variant="outline" asChild>
                     <a href={project.deployedLink}>
                       Live Demo
